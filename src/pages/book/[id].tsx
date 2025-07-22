8da6041c7714b1864eb0a46ee3aabc354eb9671c
tkznzl12 <74891586+tkznzl12@users.noreply.github.com>
2025-07-22 11:07:35 +0900

    SSR 연동

---
+++ src/pages/book/[id].tsx
@@ -0,0 +1,39 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import style from "./[id].module.css";
+import fetchOneBook from "@/lib/fetch-one-books";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+  return {
+    props: { book },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) return "문제가 발생했습니다. 다시 시도하세요";
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subtitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,33 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: { books },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+//searchable Layout 적용
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
