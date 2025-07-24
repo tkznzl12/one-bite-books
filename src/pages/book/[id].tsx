4084d9f9a353b3e27801c4705e559a351ea5112b
tkznzl12 <74891586+tkznzl12@users.noreply.github.com>
2025-07-24 21:42:25 +0900

    SSG 연동

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,12 +1,38 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {
+  // GetServerSidePropsContext,
+  GetStaticPropsContext,
+  // InferGetServerSidePropsType,
+  InferGetStaticPropsType,
+} from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-books";
+import { useRouter } from "next/router";
+import { notFound } from "next/navigation";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+    //false : 404 Notfound
+    // blocking : SSR방식
+    // true : SSR 방식 + 데이터가 없는 풀백 상태의 페이지부터 반환
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: { book },
   };
@@ -14,8 +40,13 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  // 페이지가 fallback 상태에 있을때 출력시킬것
+  if (router.isFallback) return "로딩중입니다";
   if (!book) return "문제가 발생했습니다. 다시 시도하세요";
+
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,13 @@ import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
 //ssr
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+//ssg
+export const getStaticProps = async () => {
   // 페이지 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
   // 브라우저에서만 사용할 수 있는 것을 사용하면 에러가남. (해당 Props자체가 서버에서 실행되기 때문.)
+
+  console.log("인덱스 페이지");
 
   //함수 병렬 동작
   const [allBooks, recoBooks] = await Promise.all([
@@ -30,7 +34,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetStaticPropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
